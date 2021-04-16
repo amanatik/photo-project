b3e6a0bf096b125df6be6ecb66c536f180441865
Sergio <bernaldojunior@gmail.com>
2021-04-16 12:25:52 +0300

    deploy_1

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require('express')
 const mognoose = require('mongoose')
 const logger = require('morgan')
 const path = require('path')
-const submitRouter = require('./routes/submit')
 const server = express()
 const createError = require('http-errors');
+const mongodb = require('mongodb')
+const sessions = require('express-session')
+const MongoStore = require('connect-mongo');
+const MongoClient = require('mongodb').MongoClient;
+
+require('dotenv').config();
+const port = process.env.PORT;
+const dbPath = process.env.DB_HOST + process.env.DB_PORT + process.env.DB_NAME;
+const secretKey = process.env.SECRET_KEY;
+const uri = process.env.DB_ATLAS_PATH
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
+const submitRouter = require('./routes/submit')
 
-const serviceRouter = require('./routes/serviceRoute');
-const servicePackageRouter = require('./routes/servicePackageRoute')
 
 server.set('view engine', 'hbs')
 server.set('views', path.join(__dirname, 'views'));
@@ -21,15 +30,14 @@ server.use(express.urlencoded({extended: true}))
 
 
 
+
+
+
+
+
+
 server.use('/', submitRouter)
 
-
-
-
-
-
-server.use('/service', serviceRouter);
-server.use('/servicePackage' , servicePackageRouter)
 
 // Если HTTP-запрос дошёл до этой строчки, значит ни один из ранее встречаемых рутов не ответил на запрос. Это значит, что искомого раздела просто нет на сайте. Для таких ситуаций используется код ошибки 404. Создаём небольшое middleware, которое генерирует соответствующую ошибку.
 server.use((req, res, next) => {
@@ -37,9 +45,15 @@ server.use((req, res, next) => {
   next(error);
 });
 
-server.listen(3000, ()=> {
+server.listen(port, ()=> {
   console.log('Server run on port 3000...')
-  mognoose.connect('mongodb://localhost:27017/photo-project', {useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true},() => {
+  mognoose.connect(dbPath, {useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true},() => {
     console.log('Database run successfully')
   })
+  // client.connect(err => {
+  //   console.log('Что-то начудил')
+  //   const collection = client.db("test").collection("devices");
+  //   // perform actions on the collection object
+  //   client.close();
+  // });
 })
